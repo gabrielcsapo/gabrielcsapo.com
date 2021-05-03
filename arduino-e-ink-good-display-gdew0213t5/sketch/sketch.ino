116d1f8e8748750c02cefa969578e04a1458d87b
Gabriel Csapo <gabecsapo@gmail.com>
2021-05-02 17:41:51 -0700

    [blog] post arduino-e-ink-good-display-gdew0213t5 sketch

---
+++ arduino-e-ink-good-display-gdew0213t5/sketch/sketch.ino
@@ -0,0 +1,37 @@
+#include <GxEPD2_BW.h>
+#include <GxEPD2_3C.h>
+#include <Fonts/FreeMonoBold9pt7b.h>
+
+GxEPD2_BW<GxEPD2_213_M21, GxEPD2_213_M21::HEIGHT> display(GxEPD2_213_M21(/*CS=15*/ SS, /*DC=4*/ 4, /*RST=5*/ 5, /*BUSY=16*/ 16)); // GDEW0213M21
+
+void setup()
+{
+  display.init(115200);
+  helloWorld();
+  display.hibernate();
+}
+
+const char HelloWorld[] = "How are you?";
+
+void helloWorld()
+{
+  display.setRotation(1);
+  display.setFont(&FreeMonoBold9pt7b);
+  display.setTextColor(GxEPD_BLACK);
+  int16_t tbx, tby; uint16_t tbw, tbh;
+  display.getTextBounds(HelloWorld, 0, 0, &tbx, &tby, &tbw, &tbh);
+  // center the bounding box by transposition of the origin:
+  uint16_t x = ((display.width() - tbw) / 2) - tbx;
+  uint16_t y = ((display.height() - tbh) / 2) - tby;
+  display.setFullWindow();
+  display.firstPage();
+  do
+  {
+    display.fillScreen(GxEPD_WHITE);
+    display.setCursor(x, y);
+    display.print(HelloWorld);
+  }
+  while (display.nextPage());
+}
+
+void loop() {};
